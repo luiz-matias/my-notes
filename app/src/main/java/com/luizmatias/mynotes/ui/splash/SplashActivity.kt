105e2dcc6ebd9420b3b6377cd753819c00b91808
Luiz Matias <luiz.matias1999@gmail.com>
2018-10-29 16:24:26 -0300

    Migrated to AndroidX + Material Design 2.0

---
+++ app/src/main/java/com/luizmatias/mynotes/ui/splash/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.luizmatias.mynotes.ui.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.luizmatias.mynotes.R
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+
+    }
+}
